41b240414765675b2f9fc33508326b04862662eb
youwishyouhadthishandle <bachawalasupraj@gmail.com>
2020-03-25 17:02:28 +0530

    add conversion

--- synesthesia.py
+++ synesthesia.py
@@ -26,10 +26,24 @@ def synethesise(inp_image, out_wave, min_freq, max_freq, rate, scale):
 
     output = wave.open(out_wave, 'w')
     output.setparams((1, 2, rate, 0, 'NONE', 'not compressed'))
+    wave = []
 
-    interval_size = (max_freq - min_freq) / img_height
+    block_size = (max_freq - min_freq) / img_height
+    samples_per_pixel = rate//scale
+    data = array.array('h')
+
+    for i in range(img_width):
+        for j in range(img_height):
+            yinv = img_width - y - 1     
+            amplitude = img.getpixel((x,y))
+            data = genwave(yinv * block_size + min_freq, amplitude, samples_per_pixel, rate)
+            for k in range(samples_per_pixel):
+                for l in data:
+                    wave[i + x * samples_per_pixel] += l[k]
+
+
     
     
-
+    
 
 
